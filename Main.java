154c4a1a0cae37dfd67be683bc1b5f6f7c4e2f8f
yigitcanga <105357336+yigitcanga@users.noreply.github.com>
2023-11-26 12:24:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Form1 f = new Form1();
+                f.setVisible(true);
+            }
+        });
+
+    }
+}
